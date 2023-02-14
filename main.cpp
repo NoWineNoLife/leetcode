c266359a823cfd2a553622960f0943246af554a2
tsing0401 <tsing0401@outlook.com>
2023-02-14 23:11:34 +0800

    update ci

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
  */
 
 #include <bits/stdc++.h>
-#include "doctest.h"
+#include <doctest/doctest.h>
 
 #include <algorithm>
 #include <backward.hpp>
@@ -20,45 +20,41 @@
 #include "leetcode1_100.h"
 #include "leetcode601_700.h"
 
-
-namespace test_leetcode{
-    void test1_100()
-  {
-    algorithm_leetcode::Solution_1_100 solution;
-    std::vector<std::vector<int>> v{{0, 0, 1, 1},
-                                    {0, 1, 1, 0},
-                                    {0, 1, 0, 1}};
-
-    std::vector<std::vector<int>> vv{{0,0,0,0,0,0,0,0}};
-
+namespace test_leetcode
+{
 	TEST_CASE("TEST doc")
 	{
-		CHECK(solution.maxAreaOfIsland(vv) ==5);
-	}
+		algorithm_leetcode::Solution_1_100 solution;
+		std::vector<std::vector<int>> v{
+			{0, 0, 1, 1}, {0, 1, 1, 0}, {0, 1, 0, 1}};
 
-};
-}
-;
+		std::vector<std::vector<int>> vv{{0, 0, 0, 0, 0, 0, 0, 0}};
+		CHECK(solution.maxAreaOfIsland(vv) == 5);
+	}  // namespace test_leetcode
 
-int main() {
+	;
 
-    test_leetcode::test1_100();
-
-
-
-  ListNode* l1 = new ListNode{5};
-  //    l1->next = new ListNode{8};
-  ListNode* l2 = new ListNode{5};
-  //    so.addTwoNumbers(l1, l2);
-
-  std::vector<int> v_tmp{1, 3, 1, 5, 4};
-  //    std::cout << so.findPairs(v_tmp, 0) << std::endl;
-
-  algorithm_leetcode::Solution_601_700 so;
-  std::vector<std::vector<int>> v{{0, 0, 1, 1}, {0, 1, 1, 0}, {0, 1, 0, 1}};
-
-  std::vector<std::vector<int>> vv{{0, 0, 0, 0, 0, 0, 0, 0}};
-  so.maxAreaOfIsland(vv);
-
-  return 0;
-}
+}  // namespace test_leetcode
+   // int main() {
+   //
+   //     test_leetcode::test1_100();
+   //
+   //
+   //
+   //   ListNode* l1 = new ListNode{5};
+   //   //    l1->next = new ListNode{8};
+   //   ListNode* l2 = new ListNode{5};
+   //   //    so.addTwoNumbers(l1, l2);
+   //
+   //   std::vector<int> v_tmp{1, 3, 1, 5, 4};
+   //   //    std::cout << so.findPairs(v_tmp, 0) << std::endl;
+   //
+   //   algorithm_leetcode::Solution_601_700 so;
+   //   std::vector<std::vector<int>> v{{0, 0, 1, 1}, {0, 1, 1, 0}, {0, 1, 0,
+   //   1}};
+   //
+   //   std::vector<std::vector<int>> vv{{0, 0, 0, 0, 0, 0, 0, 0}};
+   //   so.maxAreaOfIsland(vv);
+   //
+   //   return 0;
+   // }
